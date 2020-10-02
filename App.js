b35abddd9588d6e949b98ea92879f8a386d6ba9b
caroline-bostwick <60565864+caroline-bostwick@users.noreply.github.com>
2020-10-02 13:01:21 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './Spotify.svg';
+import './App.css';
+
+function App(props) {
+  let albumId = props.albumId;
+  let url = "http://open.spotify.com/embed/album/" + albumId;
+  console.log(albumId)
+  if (!albumId) {
+    return <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>{props.status}</div>
+      </header>
+    </div>
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <br />
+        <iframe title="spotfy-widget" src={url} width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media">
+
+        </iframe>
+        <button>Previous</button>
+        <button>Next</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
