c0e126da79636bbb35402c51b82ca0d048952276
caroline-bostwick <60565864+caroline-bostwick@users.noreply.github.com>
2020-10-04 00:25:38 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname);
+});
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : process.env.CLIENT_ID,
+  clientSecret : process.env.CLIENT_SECRET
+});
+
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+
+  spotifyApi.setAccessToken(data.body['access_token']);
+
+}, function(err) {
+  console.log('Something went wrong when retrieving an cess token', err.message);
+});
+
+app.get('/myendpoint', function (request, response) {
+
+  spotifyApi.searchTracks('track:Dancing Queen', {limit:1})
+      .then(function(data) {
+
+      response.send(data.body.tracks.items[0]);
+
+}, function(err) {
+  console.error(err);
+});
+});
